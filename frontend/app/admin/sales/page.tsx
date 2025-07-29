871ac2b98ac91378e5da303bb0c06b83fc2e0180
Antonella Denisse Cuenca <fuwa08@hotmail.com>
2025-07-29 17:32:15 -0300

    feat: implement admin layout, sales and products pages with navigation and transaction filtering

---
+++ frontend/app/admin/sales/page.tsx
@@ -0,0 +1,34 @@
+import TransactionFilter from "@/components/transactions/TransactionFilter"
+import Heading from "@/components/UI/Heading"
+import { getSalesByDate } from "@/src/api"
+import {
+  QueryClient,
+  dehydrate,
+  HydrationBoundary,
+} from "@tanstack/react-query"
+import { format } from "date-fns"
+
+export default async function SalesPage() {
+  const queryClient = new QueryClient()
+
+  const today = new Date()
+  const formattedDate = format(today, "yyyy-MM-dd")
+
+  await queryClient.prefetchQuery({
+    queryKey: ["sales", formattedDate],
+    queryFn: () => getSalesByDate(formattedDate),
+  })
+
+  return (
+    <>
+      <Heading>Ventas</Heading>
+      <p className="text-lg">
+        En esta sección podrás ver las ventas, utiliza el calendario para
+        filtrar por fechas
+      </p>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <TransactionFilter />
+      </HydrationBoundary>
+    </>
+  )
+}
