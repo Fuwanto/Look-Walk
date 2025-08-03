21a84b7698579b0d90d4c8d00f067927ab1d222b
Antonella Denisse Cuenca <fuwa08@hotmail.com>
2025-08-02 22:13:40 -0300

    last config frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "http", hostname: process.env.DOMAIN! },
-      { protocol: "https", hostname: process.env.DOMAIN! },
+      { protocol: "https", hostname: "look-walk.onrender.com" },
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
