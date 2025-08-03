257870e919858119e4b2468c9c838db2a96aa1bc
Antonella Denisse Cuenca <fuwa08@hotmail.com>
2025-08-02 22:40:38 -0300

    fix

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -16,7 +16,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Look&Walk",
   description: "Tienda online de zapatillas, gafas de sol y hoodies.",
-  themeColor: "#000000",
 }
 
 export default function RootLayout({
